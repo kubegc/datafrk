b9e8b8811eff63e1bc8c57b53735f568a3efdb8f
Heng WU <wuheng@iscas.ac.cn>
2021-02-07 20:34:03 +0800

    keepalve for pgl

--- src/main/java/com/github/kubesys/sql/clients/MysqlClient.java
+++ src/main/java/com/github/kubesys/sql/clients/MysqlClient.java
@@ -16,27 +16,27 @@ import com.github.kubesys.sql.KubeSQLClient;
  */
 public class MysqlClient extends KubeSQLClient {
 
-	public static final String CHECK_DATABASE  = "SELECT * FROM information_schema.SCHEMATA where SCHEMA_NAME='#DATBASE#'";
+	public static final String CHECK_MYSQL_DATABASE  = "SELECT * FROM information_schema.SCHEMATA where SCHEMA_NAME='#DATBASE#'";
 	
-	public static final String CREATE_DATABASE = "CREATE DATABASE #DATBASE# CHARACTER SET utf8 COLLATE utf8_general_ci";
+	public static final String CREATE_MYSQL_DATABASE = "CREATE DATABASE #DATBASE# CHARACTER SET utf8 COLLATE utf8_general_ci";
 	
 	
-	public static final String CHECK_TABLE     = "SELECT DISTINCT t.table_name, n.SCHEMA_NAME FROM "
+	public static final String CHECK_MYSQL_TABLE     = "SELECT DISTINCT t.table_name, n.SCHEMA_NAME FROM "
 												+ "information_schema.TABLES t, information_schema.SCHEMATA n "
 												+ "WHERE t.table_name = '#TABLE#' AND n.SCHEMA_NAME = '#DATBASE#'";
 	
-	public static final String CREATE_TABLE    = "CREATE TABLE #TABLE# (name varchar(512), namespace varchar(128), time bigint,"
+	public static final String CREATE_MYSQL_TABLE    = "CREATE TABLE #TABLE# (name varchar(512), namespace varchar(128), time bigint,"
 												+ "data json, primary key(name, namespace)) DEFAULT CHARSET=utf8";
 	
-	private static final String CONDITION = " JSON_EXTRACT(data, '$.#ITEM#') like '%#VALUE#%' AND ";
+	private static final String MYSQL_CONDITION = " JSON_EXTRACT(data, '$.#ITEM#') like '%#VALUE#%' AND ";
 	
-	private static final String LIMIT = " LIMIT #FROM#, #TO#";
+	private static final String MYSQL_LIMIT = " LIMIT #FROM#, #TO#";
 	
-	public static final String DEF_DRV  = "com.mysql.cj.jdbc.Driver";
+	public static final String DEF_MYSQL_DRV  = "com.mysql.cj.jdbc.Driver";
 
-	public static final String DEF_URL  = "jdbc:mysql://kube-database.kube-system:3306?useUnicode=true&characterEncoding=UTF8&connectTimeout=2000&socketTimeout=6000&autoReconnect=true&&serverTimezone=Asia/Shanghai";
+	public static final String DEF_MYSQL_URL  = "jdbc:mysql://kube-database.kube-system:3306?useUnicode=true&characterEncoding=UTF8&connectTimeout=2000&socketTimeout=6000&autoReconnect=true&&serverTimezone=Asia/Shanghai";
 
-	public static final String DEF_USER = "root";
+	public static final String DEF_MYSQL_USER = "root";
 
 	public MysqlClient(DruidPooledConnection conn) throws Exception {
 		super(conn);
@@ -48,43 +48,42 @@ public class MysqlClient extends KubeSQLClient {
 
 	@Override
 	public String checkDatabaseSql() {
-		return CHECK_DATABASE;
+		return CHECK_MYSQL_DATABASE;
 	}
 
 	@Override
 	public String createDatabaseSql() {
-		return CREATE_DATABASE;
+		return CREATE_MYSQL_DATABASE;
 	}
 
 	@Override
 	public String checkTableSql() {
-		return CHECK_TABLE;
+		return CHECK_MYSQL_TABLE;
 	}
 
 	@Override
 	public String createTableSql() {
-		return CREATE_TABLE;
+		return CREATE_MYSQL_TABLE;
 	}
 
 	@Override
 	public String defaultDriver() {
-		return DEF_DRV;
+		return DEF_MYSQL_DRV;
 	}
 
 	@Override
 	public String defaultUrl() {
-		return DEF_URL;
+		return DEF_MYSQL_URL;
 	}
 
 	@Override
 	public String defaultUser() {
-		return DEF_USER;
+		return DEF_MYSQL_USER;
 	}
-
 
 	@Override
 	public String queryConditions() {
-		return CONDITION;
+		return MYSQL_CONDITION;
 	}
 
 	
@@ -97,7 +96,7 @@ public class MysqlClient extends KubeSQLClient {
 	public String queryLimit(int limit, int page) {
 		int l = (limit <= 0) ? 10 : limit;
 		int p = (page <= 1) ? 1 : page;
-		return LIMIT.replace("#FROM#", String.valueOf((p - 1) * l)).replace("#TO#", String.valueOf(l));
+		return MYSQL_LIMIT.replace("#FROM#", String.valueOf((p - 1) * l)).replace("#TO#", String.valueOf(l));
 	}
 
 
--- src/main/java/com/github/kubesys/sql/clients/PostgreClient.java
+++ src/main/java/com/github/kubesys/sql/clients/PostgreClient.java
@@ -16,26 +16,27 @@ import com.github.kubesys.sql.KubeSQLClient;
  */
 public class PostgreClient extends KubeSQLClient {
 
-	public static final String CHECK_DATABASE = "SELECT u.datname  FROM pg_catalog.pg_database u where u.datname='#DATBASE#'";
+	public static final String CHECK_POSTGRE_DATABASE = "SELECT u.datname  FROM pg_catalog.pg_database u where u.datname='#DATBASE#'";
 
-	public static final String CREATE_DATABASE = "CREATE DATABASE #DATBASE# ENCODING = 'UTF8'";
+	public static final String CREATE_POSTGRE_DATABASE = "CREATE DATABASE #DATBASE# ENCODING = 'UTF8'";
 
-	public static final String CHECK_TABLE = "select count(*) from pg_class where relname = '#TABLE#'";
+	public static final String CHECK_POSTGRE_TABLE = "select count(*) from pg_class where relname = '#TABLE#'";
 
-	public static final String CREATE_TABLE = "CREATE TABLE #TABLE# (name varchar(512), namespace varchar(128), time int8,"
+	public static final String CREATE_POSTGRE_TABLE = "CREATE TABLE #TABLE# (name varchar(512), namespace varchar(128), time int8,"
 																+ "data json, primary key(name, namespace))";
 
-	private static final String CONDITION = " data#ITEM# like '%#VALUE#%' AND ";
+	private static final String POSTGRE_CONDITION = " data#ITEM# like '%#VALUE#%' AND ";
 
-	private static final String LIMIT = " LIMIT #LIMIT# OFFSET #OFFSET#";
+	private static final String POSTGRE_LIMIT = " LIMIT #LIMIT# OFFSET #OFFSET#";
 	
-	public static final String DEF_DRV = "org.postgresql.Driver";
+	public static final String DEF_POSTGRE_DRV = "org.postgresql.Driver";
 
-	public static final String DEF_URL = "jdbc:postgresql://kube-database.kube-system:5432/postgres"
-																+ "?useUnicode=true&characterEncoding=UTF8&connectTimeout=2000"
-																+ "&socketTimeout=6000&autoReconnect=true&&serverTimezone=Asia/Shanghai";
+	// https://jdbc.postgresql.org/documentation/94/connect.html
+	public static final String DEF_POSTGRE_URL = "jdbc:postgresql://kube-database.kube-system:5432/postgres"
+																+ "?useUnicode=true&characterEncoding=UTF8&connectTimeout=02000"
+																+ "&socketTimeout=06000&tcpKeepAlive=true&&serverTimezone=Asia/Shanghai";
 
-	public static final String DEF_USER = "postgres";
+	public static final String DEF_POSTGRE_USER = "postgres";
 
 	public PostgreClient(DruidPooledConnection conn) throws Exception {
 		super(conn);
@@ -47,37 +48,37 @@ public class PostgreClient extends KubeSQLClient {
 
 	@Override
 	public String checkDatabaseSql() {
-		return CHECK_DATABASE;
+		return CHECK_POSTGRE_DATABASE;
 	}
 
 	@Override
 	public String createDatabaseSql() {
-		return CREATE_DATABASE;
+		return CREATE_POSTGRE_DATABASE;
 	}
 
 	@Override
 	public String checkTableSql() {
-		return CHECK_TABLE;
+		return CHECK_POSTGRE_TABLE;
 	}
 
 	@Override
 	public String createTableSql() {
-		return CREATE_TABLE;
+		return CREATE_POSTGRE_TABLE;
 	}
 
 	@Override
 	public String defaultDriver() {
-		return DEF_DRV;
+		return DEF_POSTGRE_DRV;
 	}
 
 	@Override
 	public String defaultUrl() {
-		return DEF_URL;
+		return DEF_POSTGRE_URL;
 	}
 
 	@Override
 	public String defaultUser() {
-		return DEF_USER;
+		return DEF_POSTGRE_USER;
 	}
 
 	@Override
@@ -87,14 +88,14 @@ public class PostgreClient extends KubeSQLClient {
 
 	@Override
 	public String queryConditions() {
-		return CONDITION;
+		return POSTGRE_CONDITION;
 	}
 
 	@Override
 	public String queryLimit(int limit, int page) {
 		int l = (limit <= 0) ? 10 : limit;
 		int p = (page <= 1) ? 1 : page;
-		return LIMIT.replace("#LIMIT#", String.valueOf(limit)).replace("#OFFSET#", String.valueOf((p - 1) * l));
+		return POSTGRE_LIMIT.replace("#LIMIT#", String.valueOf(limit)).replace("#OFFSET#", String.valueOf((p - 1) * l));
 	}
 
 }
